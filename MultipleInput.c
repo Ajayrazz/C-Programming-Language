a50e3cacf59b83e224ca7989dff5f3c643d4dc56
AJAY RAZZ <149960524+Ajayrazz@users.noreply.github.com>
2024-01-03 16:48:05 +0530

    C program to take multiple input in a single line.

---
+++ MultipleInput.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("enter value: \n");
+    int num1,num2,num3;
+    scanf("%d%d%d",&num1,&num2,&num3);
+    printf("Given input: %d %d %d",num1,num2,num3);
+    return 0;
+}
