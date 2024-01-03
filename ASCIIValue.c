a949b3c00579fa4a6a6e34206224dfcf1c4e3331
AJAY RAZZ <149960524+Ajayrazz@users.noreply.github.com>
2024-01-03 17:10:30 +0530

    C program to find the ASCII Value of the input character.

---
+++ ASCIIValue.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("Enter a character: \n");
+    char val;
+    scanf("%c",&val);
+    printf("ASCII value of the given character: %d",val);
+    return 0;
+}
