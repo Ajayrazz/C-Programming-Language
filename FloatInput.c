319af48bb7d0d83ea59c7fade90375a017b540ff
AJAY RAZZ <149960524+Ajayrazz@users.noreply.github.com>
2024-01-03 16:01:07 +0530

    C program to take float input from user.

---
+++ FloatInput.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("Enter float value : \n");
+    float a,b;
+    scanf("%f",&a);
+    scanf("%f",&b);
+    printf("First input value : %f\n",a);
+    printf("Second input value : %f\n",b);
+    return 0;
+}
