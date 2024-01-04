8136e067771b0e6a434919cf7ada4f3ec1bdc4fb
AJAY RAZZ <149960524+Ajayrazz@users.noreply.github.com>
2024-01-04 15:54:06 +0530

    C program to find whether the user given input integer is even or odd by using ternary operator.

---
+++ EvenOrOddUsingTernaryOperator.c
@@ -0,0 +1,10 @@
+//Using Ternary Operator find odd or even.
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter value: \n");
+    scanf("%d",&n);
+    (n%2==0)? printf("Even Number.") : printf("Odd Number.");
+    return 0;
+}
