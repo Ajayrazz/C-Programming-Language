78dd8c2a18746528b895424eea88d48b31def679
AJAY RAZZ <149960524+Ajayrazz@users.noreply.github.com>
2024-01-05 19:53:03 +0530

    C program to print ULTA NUMBER TRIANGLE by taking input from user.

---
+++ PatternUltaNumberTriangle.c
@@ -0,0 +1,15 @@
+//program to print ULTA NUMBER TRIANGLE
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter value : ");
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n+1-i; j++){
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
