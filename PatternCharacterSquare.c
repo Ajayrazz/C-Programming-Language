d121773f723b684a2481d5c54e008833e1796c44
AJAY RAZZ <149960524+Ajayrazz@users.noreply.github.com>
2024-01-06 13:16:49 +0530

    C program to print pattern of character solid square

---
+++ PatternCharacterSquare.c
@@ -0,0 +1,17 @@
+//program to print pattern of character solid square 
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a value : ");
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++){
+        char ch = 'A';
+        for(int j=1; j<=n; j++){
+            printf("%c",ch);
+            ch++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
