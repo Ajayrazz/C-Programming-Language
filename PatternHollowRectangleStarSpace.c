e5b229da27f03277f62a3dcae5244052adba36a2
AJAY RAZZ <149960524+Ajayrazz@users.noreply.github.com>
2024-01-06 14:30:43 +0530

    C Program to print a pattern of hollow rectangle of star and space.

---
+++ PatternHollowRectangleStarSpace.c
@@ -0,0 +1,20 @@
+//Program to print a pattern of hollow rectangle of star and space
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a value : ");
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i==1 || j==1 || i==n || j==n){
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
